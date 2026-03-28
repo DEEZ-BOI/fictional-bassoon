ec7646c2d2d1e08f7c131c471046a979938dd33d
Suyash Prasad <130206154+DEEZ-BOI@users.noreply.github.com>
2026-03-28 15:09:03 +0530

    Implement print_maze_with_path function
    Add function to print maze with path marked

--- maze_solver/visualize.py
+++ maze_solver/visualize.py
@@ -1,1 +1,9 @@
+def print_maze_with_path(maze, path):
+    maze_copy = [row[:] for row in maze]
 
+    for x, y in path:
+        if maze_copy[x][y] not in ['S', 'G']:
+            maze_copy[x][y] = '*'
+
+    for row in maze_copy:
+        print(" ".join(row))
