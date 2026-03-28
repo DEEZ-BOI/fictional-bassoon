973c3890d39cf7b9a6f72352f7f4b2c48b5b6325
Suyash Prasad <130206154+DEEZ-BOI@users.noreply.github.com>
2026-03-28 15:05:59 +0530

    Implement main play function for Tic Tac Toe

--- tic_tac_toe/play.py
+++ tic_tac_toe/play.py
@@ -1,1 +1,28 @@
+from tic_tac_toe.game import print_board, check_winner, is_full, EMPTY, HUMAN, AI
+from tic_tac_toe.minmax import best_move
 
+
+def play():
+    board = [[EMPTY]*3 for _ in range(3)]
+
+    while True:
+        print_board(board)
+
+        x, y = map(int, input("Enter move (row col): ").split())
+        if board[x][y] != EMPTY:
+            print("Invalid move")
+            continue
+        board[x][y] = HUMAN
+
+        if check_winner(board) or is_full(board):
+            break
+
+        move = best_move(board)
+        if move:
+            board[move[0]][move[1]] = AI
+
+        if check_winner(board) or is_full(board):
+            break
+
+    print_board(board)
+    print("Winner:", check_winner(board))
