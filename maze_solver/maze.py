319c7f3504c28fd915dd53cb4391a6f979e72b1a
Suyash Prasad <130206154+DEEZ-BOI@users.noreply.github.com>
2026-03-28 15:07:20 +0530

    Add function to get neighboring positions in maze

--- maze_solver/maze.py
+++ maze_solver/maze.py
@@ -1,1 +1,11 @@
+def get_neighbors(pos, maze):
+    x, y = pos
+    moves = [(0,1),(1,0),(0,-1),(-1,0)]
+    neighbors = []
 
+    for dx, dy in moves:
+        nx, ny = x+dx, y+dy
+        if 0 <= nx < len(maze) and 0 <= ny < len(maze[0]) and maze[nx][ny] != '#':
+            neighbors.append((nx, ny))
+
+    return neighbors
