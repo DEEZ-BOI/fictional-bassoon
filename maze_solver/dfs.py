629a9b4894bd618baa5813b172b3c7922e200742
Suyash Prasad <130206154+DEEZ-BOI@users.noreply.github.com>
2026-03-28 15:07:00 +0530

    Add DFS algorithm to solve mazes
    Implement depth-first search algorithm for maze solving.

--- maze_solver/dfs.py
+++ maze_solver/dfs.py
@@ -1,1 +1,22 @@
+from maze_solver.maze import get_neighbors
 
+
+def dfs(maze, start, goal):
+    stack = [(start, [start])]
+    visited = set()
+
+    while stack:
+        node, path = stack.pop()
+
+        if node == goal:
+            return path
+
+        if node in visited:
+            continue
+
+        visited.add(node)
+
+        for neighbor in get_neighbors(node, maze):
+            stack.append((neighbor, path + [neighbor]))
+
+    return None
